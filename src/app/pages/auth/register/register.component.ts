aa99669b6c83b9bcfc227d3b7957b13a40b4e310
SeanPalomares <u202121947@upc.edu.pe>
2024-11-04 16:56:48 -0500

    feature: se realizó el registro

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule,MatButtonModule,MatInputModule,MatCardModule,MatSnackBarModule,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  
+  registerForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
 
+  constructor(){
+    this.registerForm=this.fb.group({
+      firstName:['',[Validators.required]],
+      lastName:['',[Validators.required]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]]
+    });
+  }
+
+  controlHasError(control:string, error:string){
+    return this.registerForm.controls[control].hasError(error);
+  }
+  showSnackBar(message:string) {
+    this.snackBar.open(message,'Cerrar',{
+      duration:3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  } 
+
+  onSubmit(){
+    if(this.registerForm.valid){
+      const credentials = this.registerForm.value;
+      console.log('Credenciales:', credentials);
+      this.showSnackBar('Inicio de sesión exitoso');
+    }
+  }
 }
