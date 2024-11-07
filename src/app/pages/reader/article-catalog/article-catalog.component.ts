a952deae62ee6e6a66f051122bb8a7777ead95cc
Seroluce <141604179+Seroluce@users.noreply.github.com>
2024-11-06 22:47:42 -0500

    Añadir filtro de categorías en Catálogo de Artículos
    Se agregó un filtro de categorías en el componente `article-catalog` para mejorar la búsqueda de artículos. También se actualizaron los estilos CSS y se limpiaron líneas innecesarias en el HTML.

--- src/app/pages/reader/article-catalog/article-catalog.component.ts
+++ src/app/pages/reader/article-catalog/article-catalog.component.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { ArticleCardComponent } from '../../../shared/components/article-card/article-card.component';
 import { ArticleService } from '../../../core/services/article.services';
 import { Article } from '../../../shared/models/article.model';
+import {CategoryService} from '../../../core/services/category.services';
+import {Category} from '../../../shared/models/category.model';
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {MatDivider} from '@angular/material/divider';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-article-catalog',
   standalone: true,
-  imports: [CommonModule, FormsModule, ArticleCardComponent, MatCard, MatCardContent, MatDivider],
+  imports: [CommonModule, FormsModule, ArticleCardComponent, MatCard, MatCardContent, MatDivider, MatButtonToggleGroup, MatButtonToggle],
   templateUrl: './article-catalog.component.html',
   styleUrl: './article-catalog.component.css'
 })
@@ -18,8 +21,10 @@ export class ArticleCatalogComponent {
   articles: Article[] = [];
   filteredArticles: Article[] = [];
   searchQuery: string = '';
+  categories: Category[] = [];
 
   private articleService = inject(ArticleService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit(): void {
     this.loadArticles();
@@ -27,6 +32,7 @@ export class ArticleCatalogComponent {
 
   loadArticles(): void {
     this.articles = this.articleService.getArticles();
+    this.categories = this.categoryService.getCategories();
     this.filteredArticles = [...this.articles];
   }
 
@@ -35,5 +41,16 @@ export class ArticleCatalogComponent {
       article.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
       article.category.name.toLowerCase().includes(this.searchQuery.toLowerCase())
     );
+  }
+
+  filterArticlesByCategories(event: any): void {
+    const selectedCategories = event.value;
+    this.filteredArticles = this.articles.filter(article =>
+      selectedCategories.includes(article.category.id)
+    );
+    // Aplicar la búsqueda por título o categoría si hay una consulta de búsqueda activa
+    if (this.searchQuery) {
+      this.filterArticles();
+    }
   }
 }
--- src/app/shared/components/article-card/article-card.component.ts
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import {MatButton} from '@angular/material/button';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, RouterLink, MatButton],
+  imports: [CommonModule, MatCardModule, RouterLink, MatButton, MatDivider],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.css'
 })
 export class ArticleCardComponent {
   @Input() articles: Article[] = [];
 }
+
+
