783ef27094f80e4f7e9a7f483ebdeb81f7a0f70c
SeanPalomares <u202121947@upc.edu.pe>
2024-11-10 00:05:45 -0500

    Se añade opción de desarrollo para login rápido
    Se añade los botones login lector y escritor que permiten logearse directamente como un escritor o lector para facilitar las pruebas y desarrollo de las funciones

---
+++ src/app/pages/auth/auth-fastreader/auth-fastreader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-fastreader',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-fastreader.component.html',
+  styleUrl: './auth-fastreader.component.css'
+})
+export class AuthFastreaderComponent {
+
+}
---
+++ src/app/pages/auth/auth-fastwriter/auth-fastwriter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-fastwriter',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-fastwriter.component.html',
+  styleUrl: './auth-fastwriter.component.css'
+})
+export class AuthFastwriterComponent {
+
+}
