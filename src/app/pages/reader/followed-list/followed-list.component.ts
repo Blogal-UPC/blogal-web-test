5e605ab5f1589300a895b996b72e55df2b6c8fd9
LiamQuinoNeff <liamquinoneff@gmail.com>
2024-11-16 00:33:28 -0500

    feat: Apartado de creación seguidos creado con links a publicaciones

--- src/app/pages/reader/followed-list/followed-list.component.ts
+++ src/app/pages/reader/followed-list/followed-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-followed-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './followed-list.component.html',
-  styleUrl: './followed-list.component.css'
+  styleUrls: ['./followed-list.component.css']
 })
 export class FollowedListComponent {
-
+  followedWriters = [
+    { name: 'John Doe', publicationsUrl: '/reader/catalog' },
+    { name: 'Juan Solis', publicationsUrl: '/reader/catalog' },
+    // Añadir más escritores según sea necesario
+  ];
 }
