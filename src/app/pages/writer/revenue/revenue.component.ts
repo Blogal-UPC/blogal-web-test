029519e506541c30c3f92a5882f213ffde3b3e54
rchui17 <103223281+rchui17@users.noreply.github.com>
2024-11-16 00:23:23 -0500

    Feat: Tabla de Ingresos Mensuales

--- src/app/pages/writer/revenue/revenue.component.ts
+++ src/app/pages/writer/revenue/revenue.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-revenue',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './revenue.component.html',
   styleUrl: './revenue.component.css'
 })
 export class RevenueComponent {
-
+  revenues = [
+    { month: 'January', donationsCount: 2, total: 5 },
+    { month: 'February', donationsCount: 8, total: 20 },
+    { month: 'March', donationsCount: 22, total: 55 },
+  ];
 }
