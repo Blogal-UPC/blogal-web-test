09edbfd84a839d7fbe4248e0925b0be569ac29ed
Seroluce <141604179+Seroluce@users.noreply.github.com>
2024-11-16 17:46:30 -0500

    **refactor: Simplificar código y mejorar estilos de interfaz de la vista de detalles de artículo**
    Elimina divisores y ajusta importaciones en article-card para reducir redundancia. Modificar funcionalidad de guardar artículo y mejora la interfaz en article-detail al incluir una barra de herramientas y botones de acción. Añade estilos SCSS para una mejor presentación visual en ambas componentes, integrando colores del tema de Angular Material.

--- src/app/shared/components/article-card/article-card.component.ts
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -1,62 +1,55 @@
-import { Component, inject, Input } from '@angular/core';
-import { Article } from '../../models/article.model';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
+import {Component, inject, Input} from '@angular/core';
+import {Article} from '../../models/article.model';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {RouterLink} from '@angular/router';
 import {MatButton} from '@angular/material/button';
-import {MatDivider} from '@angular/material/divider';
-import { ArticleSaveService } from '../../../core/services/article-save.service';
-import { AuthService } from '../../../core/services/auth.service';
-import { User } from '../../models/user.model';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import {ArticleSaveService} from '../../../core/services/article-save.service';
+import {AuthService} from '../../../core/services/auth.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, RouterLink, MatButton, MatDivider],
+  imports: [CommonModule, MatCardModule, RouterLink, MatButton],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.css'
 })
 export class ArticleCardComponent {
-  @Input() articles: Article[] = []; 
+  @Input() articles: Article[] = [];
   @Input() showSaveButton: boolean = true;
-  private articleSaveService=inject(ArticleSaveService);
-  private authService=inject(AuthService);
+  private articleSaveService = inject(ArticleSaveService);
+  private authService = inject(AuthService);
   private snackBar = inject(MatSnackBar);
 
-  showSnackBar(message:string) {
-    this.snackBar.open(message,'Cerrar',{
-      duration:3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'bottom',
+  showSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000, horizontalPosition: 'center', verticalPosition: 'bottom',
     });
   }
-  addArticleSave(article: Article){
-    const user=this.authService.getcurrentUser();
-    if(user){
-      this.articleSaveService.addArticleSave(user,article);
-      this.showSnackBar('Artículo guardado para ver más tarde');
-    }
-    else{
 
+  addArticleSave(article: Article) {
+    const user = this.authService.getcurrentUser();
+    if (user) {
+      this.articleSaveService.addArticleSave(user, article);
+      this.showSnackBar('Artículo guardado para ver más tarde');
+    } else {
       this.showSnackBar('no hay usuario');
     }
   }
-  deleteArticleSave(article: Article){
-    const user=this.authService.getcurrentUser();
-    
-    if(user){
-      if(this.articleSaveService.deleteArticleSave(user,article)){
+
+  deleteArticleSave(article: Article) {
+    const user = this.authService.getcurrentUser();
+
+    if (user) {
+      if (this.articleSaveService.deleteArticleSave(user, article)) {
         this.showSnackBar('Se ha eliminado el artículo de guardados');
-        this.articles = this.articles.filter(a=>a.id!==article.id);
-      }
-      else{
+        this.articles = this.articles.filter(a => a.id !== article.id);
+      } else {
         this.showSnackBar('No se ha podido eliminar el artículo de guardados');
       }
-    }
-    else{
+    } else {
 
       this.showSnackBar('Error, no se pudo obtener el usuario actual');
     }
--- src/app/shared/components/article-detail/article-detail.component.ts
+++ src/app/shared/components/article-detail/article-detail.component.ts
@@ -5,19 +5,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ArticleService } from '../../../core/services/article.services';
 import { Tag } from '../../models/tag.model';
 import { TagService } from '../../../core/services/tag.services';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {ArticleSaveService} from '../../../core/services/article-save.service';
+import {AuthService} from '../../../core/services/auth.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-article-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatIconButton, MatCard, MatCardContent],
   templateUrl: './article-detail.component.html',
-  styleUrl: './article-detail.component.css'
-  
-  
+  styleUrl: './article-detail.component.scss'
+
+
 })
 export class ArticleDetailComponent {
-
   article: Article | null = null;
   private route = inject(ActivatedRoute);
   private articleService = inject(ArticleService);
@@ -26,6 +32,10 @@ export class ArticleDetailComponent {
   private location = inject(Location);
 
   tags_list: Tag[]=[];
+
+  private articleSaveService = inject(ArticleSaveService);
+  private authService = inject(AuthService);
+  private snackBar = inject(MatSnackBar);
 
   ngOnInit(): void {
     const articleId = Number(this.route.snapshot.paramMap.get('id'));
@@ -36,6 +46,23 @@ export class ArticleDetailComponent {
       tags?.forEach((tag) =>{
         this.tags_list.push(this.tagService.getTagById(tag)!);
       });
+    }
+  }
+
+  showSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000, horizontalPosition: 'center', verticalPosition: 'bottom',
+    });
+  }
+
+  addArticleSave(article: Article){
+    const user=this.authService.getcurrentUser();
+    if(user){
+      this.articleSaveService.addArticleSave(user,article);
+      this.showSnackBar('Artículo guardado para ver más tarde');
+    }
+    else{
+      this.showSnackBar('no hay usuario');
     }
   }
 
