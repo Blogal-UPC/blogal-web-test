7f583e78c4f7cd3d8cf562aa0d40658fdac04381
rchui17 <103223281+rchui17@users.noreply.github.com>
2024-11-16 22:56:37 -0500

    Feat: Notificaciones para el escritor

--- src/app/pages/writer/notifications/notifications.component.ts
+++ src/app/pages/writer/notifications/notifications.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
 export class NotificationsComponent {
+  notifications = [
+    { message: 'Rafael Chui le ha donado 2$' },
+    { message: 'Axel Pariona le ha donado 5$' },
+    { message: 'Marsi Figueroa le ha donado 3$' },
+    { message: 'Rodrigo Meza le ha donado 3$' }
+  ];
 
+  markAsRead(index: number): void {
+    this.notifications.splice(index, 1);
+  }
+
+  clearAllNotifications(): void {
+    this.notifications = [];
+  }
 }
